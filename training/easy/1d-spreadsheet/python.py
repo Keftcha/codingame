476447cbcf8c562f83144ecf630e361a435621a3
Keftcha <alban.dewilde@gmail.com>
2019-07-03 11:23:10 +0200

    Add 1d-spreadsheet puzzle pythons solutions

---
+++ training/easy/1d-spreadsheet/python.py
@@ -0,0 +1,39 @@
+import operator
+
+def operate(idx, sheet):
+    if sheet[idx][0] == "VALUE":
+        return sheet[idx][1]
+    elif sheet[idx][0] == "ADD":
+        function = operator.add
+    elif sheet[idx][0] == "SUB":
+        function = operator.sub
+    elif sheet[idx][0] == "MULT":
+        function = operator.mul
+    
+    return function(int(sheet[idx][1]), int(sheet[idx][2]))
+
+
+def resolve(idx, sheet, results):
+    # the first args is a reference
+    if "$" in sheet[idx][1]:
+        sheet[idx][1] = resolve(int(sheet[idx][1][1:]), sheet, results)
+    # the second args is a reference
+    if "$" in sheet[idx][2]:
+        sheet[idx][2] = resolve(int(sheet[idx][2][1:]), sheet, results)
+    
+    results[idx] = str(operate(idx, sheet))
+    
+    return results[idx]
+        
+
+n = int(input())
+spreadsheet = []
+for i in range(n):
+    spreadsheet.append(raw_input().split())
+    
+results = ["*"] * n
+
+while "*" in results:
+    resolve(results.index("*"), spreadsheet, results)
+    
+print("\n".join(results))
