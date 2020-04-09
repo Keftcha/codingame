422aac4c8af840de526408ec8671c3aa9a574004
Keftcha <alban.dewilde@gmail.com>
2020-04-09 11:46:44 +0200

    Improve chuck-norris-code-golf puzzle python3 solution

--- multiplayer/codegolf/chuck-norris-codesize/python3.py
+++ multiplayer/codegolf/chuck-norris-codesize/python3.py
@@ -1,7 +1,8 @@
-b="".join(["{:07b}".format(ord(l))for l in input()])
-f=lambda d,f,c:("00 "if c=="0"else"0 ")+(f-d)*"0"
-c=""
-p=0
-for i,e in enumerate(b):
- if e!=b[p]:c+=f(p,i,b[p])+" ";p=i
-print(c+f(p,i+1,b[p]))
+r=lambda l:"{:07b}".format(ord(l))
+m="".join(map(r,list(input())))
+p=""
+o=""
+for e in m:
+ if e!=p:o+=(" 00 "," 0 ")[int(e)];p=e
+ o+="0"
+print(o[1:])
