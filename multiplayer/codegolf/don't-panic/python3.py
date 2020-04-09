3b69abd9851c8ce27c86d497e9473ee31be10dce
Keftcha <alban.dewilde@gmail.com>
2020-04-09 10:19:46 +0200

    Improve don't-panic-code-golf puzzle python3 solution

--- multiplayer/codegolf/don't-panic/python3.py
+++ multiplayer/codegolf/don't-panic/python3.py
@@ -1,11 +1,5 @@
-d={}
-l=[int(i)for i in input().split()]
-for i in range(l[7]):
- a,b=input().split()
- d[int(a)]=int(b)
-while 1:
- k,m,v=input().split()
- m=int(m)
- try:f=d[int(k)]
- except:f=l[4]
- print("BLOCK"if m<f and v=="LEFT"or m>f and v=="RIGHT"else"WAIT")
+i,t=int,input
+_,_,_,a,b,_,_,n=map(i,t().split())
+e={a:b}
+for _ in range(n):f,p=map(i,t().split());e[f]=p 
+while 1:f,p,d=t().split();f=i(f);p=i(p);b=(f>-1and((p<e[f]and d=="LEFT")or(e[f]<p and d=="RIGHT")));print("BLOCK"if b else"WAIT")
