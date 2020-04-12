6410a5c0d151e16853b3295a78f7fd5dd21b29d5
Keftcha <alban.dewilde@gmail.com>
2020-04-12 21:04:54 +0200

    Improve temperature-code-golf puzzle bash solution

--- multiplayer/codegolf/temperature-code-golf/bash.sh
+++ multiplayer/codegolf/temperature-code-golf/bash.sh
@@ -1,7 +1,7 @@
 read
-read tp
-for t in ${tp[@]};do
-if [ -z $m ]||[[ ${t#-} -lt ${m#-} ]]||[[ ${t#-} -eq ${m#-}&&$m -lt $t ]];then
+read e
+for t in ${e[@]};do
+if [ -z $m ]||[[ ${t#-} -lt ${m#-} ]]||[[ ${t#-} == ${m#-}&&$m < $t ]];then
 m=$t;fi;done
 [[ -z $m ]]&&m=0
 echo $m
